4c3bf2c078c43d30c237b3a9ab58dc98d357ef8b
wangjngCN <289363142@qq.com>
2017-04-17 15:53:13 +0800

    update
    Signed-off-by: wangjngCN <289363142@qq.com>

--- app/api/main/error.py
+++ app/api/main/error.py
@@ -6,4 +6,9 @@ from flask import render_template
 
 @main.app_errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('errpage/404.html'), 404
+
+
+@main.app_errorhandler(500)
+def page_not_found(e):
+    return render_template('errpage/500.html'), 500
