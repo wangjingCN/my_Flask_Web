2974273055672d447c22f93eba775c70d1e6695b
wangjngCN <289363142@qq.com>
2017-04-17 15:19:04 +0800

    update
    Signed-off-by: wangjngCN <289363142@qq.com>

---
+++ app/api/main/error.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from . import main
+from  flask import render_template
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
