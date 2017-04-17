7d85abe304af91c2326201cca0df338ea4633893
wangjngCN <289363142@qq.com>
2017-04-17 17:24:00 +0800

    update

--- app/api/auth/views.py
+++ app/api/auth/views.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import render_template
-from flask import request
+from flask import render_template, request, flash, redirect, url_for
 from . import auth
+from ...models import User
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user is not None and user.verify_password(form.password.data):
-            login_user(user, form.remember_me.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
-        flash('Invalid username or password.')
-    return render_template('auth/login.html', form=form)
+    if request.form and request.form['userName'] and request.form['password']:
+        user = User.query.filter_by(username=request.form['userName'])
+        if user and user.verify_password(request.form['password']):
+            pass
+        flash('invald username or password')
+
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(email=form.email.data).first()
+    #     if user is not None and user.verify_password(form.password.data):
+    #         login_user(user, form.remember_me.data)
+    #         return redirect(request.args.get('next') or url_for('main.index'))
+    #     flash('Invalid username or password.')
+    return render_template('auth/login.html')
 
 
 @auth.route('/logout')
-@login_required
+# @login_required
 def logout():
-    logout_user()
-    flash('You have been logged out.')
+    # logout_user()
+    # flash('You have been logged out.')
     return redirect(url_for('main.index'))
 
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(email=form.email.data,
-                    username=form.username.data,
-                    password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        token = user.generate_confirmation_token()
-        send_email(user.email, 'Confirm Your Account',
-                   'auth/email/confirm', user=user, token=token)
-        flash('A confirmation email has been sent to you by email.')
-        return redirect(url_for('auth.login'))
-    return render_template('auth/register.html', form=form)
+    # form = RegistrationForm()
+    # if form.validate_on_submit():
+    #     user = User(email=form.email.data,
+    #                 username=form.username.data,
+    #                 password=form.password.data)
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     token = user.generate_confirmation_token()
+    #     send_email(user.email, 'Confirm Your Account',
+    #                'auth/email/confirm', user=user, token=token)
+    #     flash('A confirmation email has been sent to you by email.')
+    #     return redirect(url_for('auth.login'))
+    return render_template('auth/register.html')
