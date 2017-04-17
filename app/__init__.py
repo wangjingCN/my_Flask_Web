0464fbc5452badee67728f47f8f7ba46baf8126e
wangjngCN <289363142@qq.com>
2017-04-17 16:48:16 +0800

    update
    Signed-off-by: wangjngCN <289363142@qq.com>

--- app/__init__.py
+++ app/__init__.py
@@ -23,5 +23,7 @@ def create_app(config_name):
     from api.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    return app
+    from api.auth import auth
+    app.register_blueprint(auth)
 
+    return app
