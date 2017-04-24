25e620b2c64d8bc39f52346362030bd595d1492c
wangjngCN <289363142@qq.com>
2017-04-24 09:44:18 +0800

    upda

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import config
 from loggingHelper import getLogger
@@ -11,6 +12,7 @@ bcrypt = Bcrypt()
 # from flask_admin import Admin
 
 db = SQLAlchemy()
+loginmanager = LoginManager()
 
 
 def create_app(config_name):
@@ -19,6 +21,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
     db.init_app(app)
     bcrypt.init_app(app)
+    loginmanager.init_app(app)
 
     from api.main import main
     app.register_blueprint(main)
