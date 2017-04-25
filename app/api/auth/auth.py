b0eb0e4661e2c99945b2098858e5ab38604fca89
wangjngCN <289363142@qq.com>
2017-04-25 14:11:51 +0800

    udpate

--- app/api/auth/auth.py
+++ app/api/auth/auth.py
@@ -3,7 +3,8 @@
 from flask_restful import Resource, reqparse
 from ...models import User
 from itsdangerous import TimedJSONWebSignatureSerializer
-from flask import current_app,abort
+from flask import current_app, abort
+from flask_login import login_required
 
 user_auth_token_post = reqparse.RequestParser()
 user_auth_token_post.add_argument('username', type=str, required=True, trim=True)
@@ -11,6 +12,7 @@ user_auth_token_post.add_argument('password', type=str, required=True, trim=True
 
 
 class AuthClass(Resource):
+    # @login_required
     def get(self):
         args = user_auth_token_post.parse_args()
         user = User.query.filter_by(username=args['username']).first()
@@ -18,4 +20,5 @@ class AuthClass(Resource):
             s = TimedJSONWebSignatureSerializer(current_app.config['SECRET_KEY'], expires_in=600)
             return {'token': s.dumps({"id": user.id})}
         else:
-            abort(401)
+            # abort(401)
+            return {"message": "unauthorized"}, 401
